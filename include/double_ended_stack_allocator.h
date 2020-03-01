53f9298e781c82a4c93934f30992bedade967171
akmubi <53152392+akmubi@users.noreply.github.com>
2020-03-01 19:37:53 +0500

    Update double_ended_stack_allocator.h

--- include/double_ended_stack_allocator.h
+++ include/double_ended_stack_allocator.h
@@ -18,19 +18,19 @@ extern void desa_reset			(DoubleEndedStackAllocator *allocator);
 
 // LOWER STACK
 extern void *desa_front_alloc_aligned	(DoubleEndedStackAllocator *allocator, const size_t size, const size_t alignment);
-extern void *desa_front_alloc			(DoubleEndedStackAllocator *allocator, const size_t size);
-extern void desa_front_free				(DoubleEndedStackAllocator *allocator, void *ptr);
-extern void desa_front_reset			(DoubleEndedStackAllocator *allocator);
+extern void *desa_front_alloc		(DoubleEndedStackAllocator *allocator, const size_t size);
+extern void desa_front_free		(DoubleEndedStackAllocator *allocator, void *ptr);
+extern void desa_front_reset		(DoubleEndedStackAllocator *allocator);
 
 // UPPER STACK
 extern void *desa_back_alloc_aligned	(DoubleEndedStackAllocator *allocator, const size_t size, const size_t alignment);
-extern void *desa_back_alloc			(DoubleEndedStackAllocator *allocator, const size_t size);
-extern void desa_back_free				(DoubleEndedStackAllocator *allocator, void *ptr);
-extern void desa_back_reset				(DoubleEndedStackAllocator *allocator);
+extern void *desa_back_alloc		(DoubleEndedStackAllocator *allocator, const size_t size);
+extern void desa_back_free		(DoubleEndedStackAllocator *allocator, void *ptr);
+extern void desa_back_reset		(DoubleEndedStackAllocator *allocator);
 
 // FOR DEBUBGGING
 extern const size_t desa_used_space		(DoubleEndedStackAllocator *allocator);
-extern const size_t desa_remaining_space(DoubleEndedStackAllocator *allocator);
+extern const size_t desa_remaining_space	(DoubleEndedStackAllocator *allocator);
 extern void desa_show_memory			(DoubleEndedStackAllocator *allocator);
 
 #endif	// _DOUBLE_ENDED_STACK_ALLOCATOR_
