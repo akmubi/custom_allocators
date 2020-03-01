13105e6bebe5fb5f8a01d55aa4222c481768c14b
akmubi <53152392+akmubi@users.noreply.github.com>
2020-03-01 19:32:39 +0500

    Create defines.h

---
+++ debug/defines.h
@@ -0,0 +1,18 @@
+#ifndef _DEFINES_H_
+#define _DEFINES_H_
+
+// debug.h
+
+// M_ASSERT, ASSERT
+#define ASSERTION_ENABLE
+
+// PRINT, PRINT_INT, PRINT_HEX
+#define DEBUG_MESSAGES
+
+// ignores passing NULL pointer in free functions
+#define IGNORE_NULL
+
+// fills allocated memory zero after initialization
+#define INIT_WITH_ZERO
+
+#endif	//_DEFINES_H_
