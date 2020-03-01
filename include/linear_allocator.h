eca3da7f114dec9ba6ab17217e0d477792be8a9e
akmubi <53152392+akmubi@users.noreply.github.com>
2020-03-01 19:34:23 +0500

    Update linear_allocator.h

--- include/linear_allocator.h
+++ include/linear_allocator.h
@@ -1,5 +1,5 @@
-#ifndef _LINEAR_ALLOC_H_
-#define _LINEAR_ALLOC_H_
+#ifndef _LINEAR_ALLOCATOR_H_
+#define _LINEAR_ALLOCATOR_H_
 #include <stddef.h>				// size_t
 
 typedef struct
@@ -9,14 +9,14 @@ typedef struct
 	char *end;
 } LinearAllocator;				// 6 bytes
 
-extern void la_init				(LinearAllocator *allocator, const size_t total_size);
-extern void *la_alloc_aligned	(LinearAllocator *allocator, const size_t size, const size_t alignment);
+extern void la_init			(LinearAllocator *allocator, const size_t total_size);
+extern void *la_alloc_aligned		(LinearAllocator *allocator, const size_t size, const size_t alignment);
 extern void *la_alloc			(LinearAllocator *allocator, const size_t size);
 extern void la_reset			(LinearAllocator *allocator);
 extern void la_terminate		(LinearAllocator *allocator);
 
 // FOR DEBUGGING
-extern const size_t la_used_space		(LinearAllocator *allocator);
+extern const size_t la_used_space	(LinearAllocator *allocator);
 extern const size_t la_remaining_space	(LinearAllocator *allocator);
-extern void la_show_memory				(LinearAllocator *allocator);
-#endif // _LINEAR_ALLOC_H_
+extern void la_show_memory		(LinearAllocator *allocator);
+#endif // _LINEAR_ALLOCATOR_H_
