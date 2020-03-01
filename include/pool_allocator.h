d4b0db24bc196b700bb44f87638a6d3d897ee2cc
akmubi <53152392+akmubi@users.noreply.github.com>
2020-03-01 19:36:01 +0500

    Update pool_allocator.h

--- include/pool_allocator.h
+++ include/pool_allocator.h
@@ -13,12 +13,12 @@ typedef struct
 	size_t element_size;
 } PoolAllocator;					// 8 bytes
 
-extern void pa_init			    (PoolAllocator *allocator, const size_t num_of_elements, const size_t element_size);
+extern void pa_init		(PoolAllocator *allocator, const size_t num_of_elements, const size_t element_size);
 extern void pa_init_aligned	(PoolAllocator *allocator, const size_t num_of_elements, const size_t element_size, const size_t alignment);
-extern void pa_free			    (PoolAllocator *allocator, void *ptr);
-extern void *pa_alloc		    (PoolAllocator *allocator);
-extern void pa_terminate	  (PoolAllocator *allocator);
-extern void pa_reset		    (PoolAllocator *allocator);
+extern void pa_free		(PoolAllocator *allocator, void *ptr);
+extern void *pa_alloc		(PoolAllocator *allocator);
+extern void pa_terminate	(PoolAllocator *allocator);
+extern void pa_reset		(PoolAllocator *allocator);
 
 // FOR DEBUGGING
 extern void pa_show_memory	(PoolAllocator *allocator);
